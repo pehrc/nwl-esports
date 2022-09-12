6f7633fbf0e0d7b84d22b17f44d54d2df35f5fb2
Pedro Correia <correia.pedro@outlook.com>
2022-09-12 10:29:54 -0300

    adiciona express

---
+++ server/src/server.mjs
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+app.get("/ads", (req, res) => {
+  return res.json([
+    { id: 1, name: "Anuncio 01" },
+    { id: 2, name: "Anuncio 02" },
+    { id: 3, name: "Anuncio 03" },
+  ]);
+});
+
+app.listen(3333);
