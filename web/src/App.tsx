e831ad666131ad291c677b0d136e516c62068094
Pedro Correia <correia.pedro@outlook.com>
2022-09-13 16:22:04 -0300

    :lipstick: feat: Estilizaçao CSS da apage home

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,3 +1,43 @@
+import "./styles/main.css";
+
+import logoImg from "./assets/logo-nlw-esports.svg";
+
 export const App = () => {
-  return <h1>Hello World</h1>;
+  return (
+    <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
+      <img src={logoImg} alt="" />
+      <h1 className="text-6xl text-white font-black mt-20">
+        Seu{" "}
+        <span className=" text-transparent bg-nlw-gradient bg-clip-text ">
+          duo
+        </span>
+        está aqui.
+      </h1>
+      <div className="grid grid-cols-6 gap-6 mt-16">
+        <a href="" className="relative">
+          <img src="/game-1.png" />
+        </a>
+
+        <a href="" className="relative">
+          <img src="/game-2.png" />
+        </a>
+
+        <a href="" className="relative">
+          <img src="/game-3.png" />
+        </a>
+
+        <a href="" className="relative">
+          <img src="/game-4.png" />
+        </a>
+
+        <a href="" className="relative">
+          <img src="/game-5.png" />
+        </a>
+
+        <a href="" className="relative">
+          <img src="/game-6.png" />
+        </a>
+      </div>
+    </div>
+  );
 };
