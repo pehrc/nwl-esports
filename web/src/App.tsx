4ed0f18a80a7c1375144eac461bde3118b7a58b2
Pedro Correia <correia.pedro@outlook.com>
2022-09-14 10:42:00 -0300

    :sparkles: Adiciona card encontrar duos

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,7 @@
 import "./styles/main.css";
 
+import { MagnifyingGlassPlus } from "phosphor-react";
+
 import logoImg from "./assets/logo-nlw-esports.svg";
 
 export const App = () => {
@@ -70,8 +72,21 @@ export const App = () => {
           </div>
         </a>
       </div>
-      <div className="pt-1 bg-nlw-gradient self-stretch rounded-lg overflow-hidden">
-        <div className="bg-[#2A2634] px-8 py-6 self-stretch mt-8 "></div>
+      <div className="pt-1 bg-nlw-gradient self-stretch rounded-lg mt-8 overflow-hidden">
+        <div className="bg-[#2A2634] px-8 py-6 flex justify-between items-center">
+          <div>
+            <strong className="text-white text-2xl font-black">
+              Não encountrou seu duo?
+            </strong>
+            <span className="text-zinc-400 block">
+              Publique um anúncio para encontrar novos players!
+            </span>
+          </div>
+          <button className="py-4 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded flex items-center gap-3">
+            <MagnifyingGlassPlus size={24} />
+            Publicar anúncio
+          </button>
+        </div>
       </div>
     </div>
   );
